77abeda882b1ffd279398e178c24b91781c98237
devc1234 <namdevchavan713@gmail.com>
2024-06-15 15:03:08 +0530

    vm:01:178455

---
+++ src/main/java/com/example/demo/servises/StudentService.java
@@ -0,0 +1,17 @@
+package com.example.demo.servises;
+
+import java.util.List;
+
+import com.example.demo.Entity.Student;
+
+public interface StudentService {
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	Student getStudentById(Long id);
+	
+	Student updateStudent(Student student);
+	
+	void deleteStudentById(Long id);
+}
---
+++ src/main/java/com/example/demo/servises/StudentServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.demo.servises;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Entity.Student;
+import com.example.demo.repo.StudentRepository;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+	private StudentRepository studentRepository;
+	
+	public StudentServiceImpl(StudentRepository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+
+	
+	@Override
+	public List<Student> getAllStudents() {
+		return studentRepository.findAll();
+	}
+
+	@Override
+	public Student saveStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public Student getStudentById(Long id) {
+		return studentRepository.findById(id).get();
+	}
+
+	@Override
+	public Student updateStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
+	@Override
+	public void deleteStudentById(Long id) {
+		studentRepository.deleteById(id);	
+	}
+
+
+}
